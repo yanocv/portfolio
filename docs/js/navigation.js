6b6fe2f93065f5a48a8cc8fc875590135049fb62
Yano <51319482+yanocv@users.noreply.github.com>
2021-01-14 23:48:23 +0900

    Change: Navigation default path

--- docs/js/navigation.js
+++ docs/js/navigation.js
@@ -10,13 +10,25 @@ async function gotoPage(page) {
 
 // Initialize navigation
 function initNavigation() {
-	let hash = new URL(window.location).hash.substring(1);
+	let url = new URL(window.location);
+	// Initial index redirection; Both GHP and LiveServer redirect to slash, so it is always there
+	let isLocalhost = url.hostname === "localhost" || url.hostname === "127.0.0.1";
+	let redirectTo = isLocalhost ? "index.html" : "index";
+	let pathname = url.pathname;
+	let landingPage = pathname.split("/").pop();
+	if (landingPage !== redirectTo) {
+		window.history.replaceState({}, "", `${pathname.substring(pathname.length - landingPage.length)}${redirectTo}`);
+	}
+
+	// Working with hash
+	let hash = url.hash.substring(1);
 	if (hash.length > 0) {
 		loadPage(hash);
 	} else {
 		gotoPage("intro");
 	}
 	$(window).on("hashchange", () => {
+		// New URL to get a fresh hash each time
 		loadPage(new URL(window.location).hash.substring(1));
 	});
 }
